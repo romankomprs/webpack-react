892530d3a7cc1f48356167d78195f6d31ce8f243
romankomprs <rkomprs@gmail.com>
2022-10-18 14:54:40 +0200

    feat: initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "../styles/App.css";
+
+
+
+
+const App = () => {
+  return (
+    <div>
+        <h1>My React + Webpack template</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader" // jsx, js files
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html" // npm start -> adds to -> dist/index.html
+        })
+    ],
+    devServer: {
+        host: "localhost",
+        port: process.env.PORT || 3000,
+        open: true,
+      },
+}
